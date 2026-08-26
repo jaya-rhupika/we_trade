e4526962d7997ab71addfcc7f0ab96c782c95c8d
madheshlogan <madheshlogan@gmail.com>
2026-08-26 04:33:12 +0000

    changes

--- migrations/002_create_customers.sql
+++ migrations/002_create_customers.sql
@@ -1,15 +1,14 @@
-CREATE TABLE customer (
-    customer_id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    dob DATE NOT NULL,
-    email VARCHAR(320) NOT NULL UNIQUE,
-    password_hashed VARCHAR(255) NOT NULL
+create type account_state as enum ('active', 'suspended', 'closed');
+CREATE TABLE account (
+    account_id VARCHAR(255) PRIMARY KEY,
+    customer_id VARCHAR(255) NOT NULL,
+    balance DECIMAL(19, 4) NOT NULL DEFAULT 0,
+    state account_state NOT NULL,
+    version INT NOT NULL DEFAULT 1,
+
+    CONSTRAINT chk_account_balance
+        CHECK (balance >= 0),
+
+    CONSTRAINT chk_account_version
+        CHECK (version > 0)
 );
-
-ALTER TABLE account
-    ADD CONSTRAINT fk_account_customer
-    FOREIGN KEY (customer_id)
-    REFERENCES customer (customer_id);
-
-CREATE UNIQUE INDEX uq_account_customer_id
-    ON account (customer_id);
